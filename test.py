cbe38410dc52fd8617e4f32984b38b34b38aad3e
tvhoang2012 <18520060@gm.uit.edu.vn>
2022-12-08 10:59:21 +0700

    test quantum
    draw quantum code

---
+++ test.py
@@ -0,0 +1,17 @@
+from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
+import matplotlib.pyplot as plt 
+qr = QuantumRegister(3, 'q')
+anc = QuantumRegister(1, 'ancilla')
+cr = ClassicalRegister(3, 'c')
+qc = QuantumCircuit(qr, anc, cr)
+
+qc.x(anc[0])
+qc.h(anc[0])
+qc.h(qr[0:3])
+qc.cx(qr[0:3], anc[0])
+qc.h(qr[0:3])
+qc.barrier(qr)
+qc.measure(qr, cr)
+
+qc.draw(output='mpl')
+plt.show()
