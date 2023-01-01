10e3c13559440e1cbda954630af25d96b326400b
tvhoang2012 <18520060@gm.uit.edu.vn>
2023-01-01 21:04:37 +0700

    Update test.py
    Deutsch-Jozsa Algorithm

--- test.py
+++ test.py
@@ -1,18 +1,77 @@
-from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
-import matplotlib.pyplot as plt 
-qr = QuantumRegister(3, 'q')
-anc = QuantumRegister(2, 'ancilla')
-cr = ClassicalRegister(3, 'c')
-qc = QuantumCircuit(qr, anc, cr)
+from qiskit import QuantumCircuit, transpile, Aer, IBMQ
+from qiskit.visualization import *
+#from qiskit.providers.aer import QasmSimulator
+import numpy as np
+import matplotlib.pyplot as plt
+# Loading your IBM Quantum account(s)
+#provider = IBMQ.load_account()
 
-qc.x(anc[0])
-qc.h(anc[0])
-qc.h(qr[0:3])
-qc.cx(qr[0:3], anc[0])
-qc.h(qr[0:3])
-qc.barrier(qr)
-qc.measure(qr, cr)
-# draw
-# test
-qc.draw(output='mpl')
+def dj_oracle(case, n):
+    # We need to make a QuantumCircuit object to return
+    # This circuit has n+1 qubits: the size of the input,
+    # plus one output qubit
+    oracle_qc = QuantumCircuit(n+1)
+    
+    # First, let's deal with the case in which oracle is balanced
+    if case == "balanced":
+        # First generate a random number that tells us which CNOTs to
+        # wrap in X-gates:
+        b = np.random.randint(1,2**n)
+        # Next, format 'b' as a binary string of length 'n', padded with zeros:
+        b_str = format(b, '0'+str(n)+'b')
+        # Next, we place the first X-gates. Each digit in our binary string 
+        # corresponds to a qubit, if the digit is 0, we do nothing, if it's 1
+        # we apply an X-gate to that qubit:
+        for qubit in range(len(b_str)):
+            if b_str[qubit] == '1':
+                oracle_qc.x(qubit)
+        # Do the controlled-NOT gates for each qubit, using the output qubit 
+        # as the target:
+        for qubit in range(n):
+            oracle_qc.cx(qubit, n)
+        # Next, place the final X-gates
+        for qubit in range(len(b_str)):
+            if b_str[qubit] == '1':
+                oracle_qc.x(qubit)
+
+    # Case in which oracle is constant
+    if case == "constant":
+        # First decide what the fixed output of the oracle will be
+        # (either always 0 or always 1)
+        output = np.random.randint(2)
+        if output == 1:
+            oracle_qc.x(n)
+    
+    oracle_gate = oracle_qc.to_gate()
+    oracle_gate.name = "Oracle" # To show when we display the circuit
+    return oracle_gate
+
+def dj_algorithm(oracle, n):
+    dj_circuit = QuantumCircuit(n+1, n)
+    # Set up the output qubit:
+    dj_circuit.x(n)
+    dj_circuit.h(n)
+    # And set up the input register:
+    for qubit in range(n):
+        dj_circuit.h(qubit)
+    # Let's append the oracle gate to our circuit:
+    dj_circuit.append(oracle, range(n+1))
+    # Finally, perform the H-gates again and measure:
+    for qubit in range(n):
+        dj_circuit.h(qubit)
+    
+    for i in range(n):
+        dj_circuit.measure(i, i)
+    
+    return dj_circuit
+n = 4
+oracle_gate = dj_oracle('balanced', n)
+dj_circuit = dj_algorithm(oracle_gate, n)
+#dj_circuit.draw(output='mpl')
+#plt.show()
+aer_sim = Aer.get_backend('aer_simulator')
+transpiled_dj_circuit = transpile(dj_circuit, aer_sim)
+results = aer_sim.run(transpiled_dj_circuit).result()
+answer = results.get_counts()
+plot_histogram(answer)
 plt.show()
